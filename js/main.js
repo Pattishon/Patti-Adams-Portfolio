8c31206f843d38100975ce62adb182cbaba1f98c
Pattishon <44838906+Pattishon@users.noreply.github.com>
2019-01-01 21:06:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,4 +20,67 @@ closeMenuIcon.onclick = function () {
 	menuSmall.style.display = "none";
 	
 }
+	
+// form validator - checks if the required fileds are filled and if e-mail is correct
+	
+	function displayErrors (errors, form) {
+        
+        //var p = document.createElement("p");
+        var p = form.querySelector(".form-errors");
+        
+        if ( !p ){
+            p = document.createElement("p");
+        }
+        
+        p.className = "form-errors";
+        p.innerHTML = errors.join("<br>");
+        
+        
+        form.insertBefore(p, form.children[0]);
+    }
+    
+    var form = document.querySelector("#form"), //pobiera cały formularz    
+        nameField = form.querySelector("input[name='name']"),//pobiera imię
+        emailField = form.querySelector("input[name='email']"),//pobiera email
+        messageField = form.querySelector("textarea[name='message']");//pobiera email
+        
+    form.addEventListener("submit", function(e){
+        
+       e.preventDefault();
+        
+        var errors = [];
+        
+       //sprawdzanie czy pola są uzupełnione poprawnie:
+        if ( nameField.value === "" ){
+            errors.push("Podaj imię.");
+            nameField.classList.add("invalid_form");}
+        else{
+            nameField.classList.remove("invalid_form"); 
+        }
+        
+        if ( emailField.value.indexOf("@" ) === -1 || emailField.value.indexOf(".") === -1 ){//jeżeli wisana wartość nie zawiera @ lub .
+            errors.push("Podaj poprawy adres e-mail.");
+            emailField.classList.add("invalid_form");}
+        else{
+          emailField.classList.remove("invalid_form");                      
+        }
+        
+        if ( messageField.value === "" ){
+            errors.push("Wpisz wiadomość.");
+            messageField.classList.add("invalid_form");}
+        else{
+            messageField.classList.remove("invalid_form"); 
+        }
+        
+        if( errors.length ){            
+            displayErrors(errors, form);
+        }           
+        else{
+            form.submit();
+        }
+        
+        
+            
+        
+    }, false); 
 })();
